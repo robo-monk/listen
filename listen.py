3b6b981c3bd992c55ff3d14364a96811e9e10da9
robo-monk <rrobomonk@gmail.com>
2023-02-19 14:28:29 +0100

    e2

--- listen.py
+++ listen.py
@@ -1,8 +1,16 @@
+import subprocess
+import ffmpeg 
 import whisper
+
+
+def stream_record():
+    subprocess.run("ffmpeg -f avfoundation -i ':0' output.mp3 -y", shell=True);   
 
 print("-> Loading model...")
 model = whisper.load_model("base")
 print("-> Done loading model...")
 
-result = model.transcribe("audio.m4a")
+stream_record()
+
+result = model.transcribe("output.mp3")
 print(result["text"])
